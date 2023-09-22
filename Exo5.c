0320b2274113e85c819485540cff7c02295ca68a
Morearty <interchannel.fr@gmail.com>
2023-09-22 13:44:38 +0200

    Add files via upload

--- Exo5.c
+++ Exo5.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-#include <math.h>
 
-int main(){
-    char num1, num2, num3;
-    char somme, produit, moyenne;
+int main() {
+    int num1, num2, num3;
+    int somme, produit;
+    float moyenne;
 
     printf("Entrez trois entiers : ");
     scanf("%d %d %d", &num1, &num2, &num3);
 
     somme = num1 + num2 + num3;
-    printf("Somme des entier: ",num3);
+    printf("Somme des entiers : %d\n", somme);
 
     produit = num1 * num2 * num3;
-    printf("Produit des entiers :", &produit);
+    printf("Produit des entiers : %d\n", produit);
 
-    moyenne = (num1 + num2 + num3)/3;
-    printf("Moyene des entiers: ", moyenne);
+    moyenne = (float)(num1 + num2 + num3) / 3;
+    printf("Moyenne des entiers : %.2f\n", moyenne);
+
+    return 0;
 }
